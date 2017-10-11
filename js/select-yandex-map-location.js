952d23c981b1d96b49c6d3e9ed6e570fce45cc73
Ivan Zhuravlev <me@dreddyi.ru>
2017-10-11 14:01:01 +0300

    fixing

--- js/select-yandex-map-location.js
+++ js/select-yandex-map-location.js
@@ -11,44 +11,57 @@
  *
  * @param {Object}  options
  * @param {boolean} options.draggable Marker draggable Option
- * TODO: describe other options here
+ * @param {string} options.address Search address
+ * @param {number} options.latitude Marker coordinates
+ * @param {number} options.longitude Marker coordinates
  */
-(function($) {
-    $.fn.selectLocation = function(options) {
+(function ($) {
+    $.fn.selectLocation = function (options) {
         var self = this;
-        var map;
 
         ymaps.ready(function () {
-            var myMap = new ymaps.Map('map', {
-                    center: [59.22, 39.89],
-                    zoom: 12,
-                    controls: []
-                }),
-                // Создаем экземпляр класса ymaps.control.SearchControl
-                mySearchControl = new ymaps.control.SearchControl({
-                    options: {
-                        noPlacemark: true
-                    }
-                }),
-                // Результаты поиска будем помещать в коллекцию.
-                mySearchResults = new ymaps.GeoObjectCollection(null, {
-                    hintContentLayout: ymaps.templateLayoutFactory.createClass('$[properties.name]')
-                });
+            var myMap = new ymaps.Map($(self).get(0), {
+                center: [59.22, 39.89],
+                zoom: 12,
+                controls: []
+            });
+
+            // Создаем экземпляр класса ymaps.control.SearchControl
+            var mySearchControl = new ymaps.control.SearchControl({
+                options: {
+                    noPlacemark: true
+                }
+            });
+
+            // Результаты поиска будем помещать в коллекцию.
+            var mySearchResults = new ymaps.GeoObjectCollection(null, {
+                hintContentLayout: ymaps.templateLayoutFactory.createClass('$[properties.name]')
+            });
+
             myMap.controls.add(mySearchControl);
             myMap.geoObjects.add(mySearchResults);
+
             // При клике по найденному объекту метка становится красной.
             mySearchResults.events.add('click', function (e) {
                 e.get('target').options.set('preset', 'islands#redIcon');
             });
+
             // Выбранный результат помещаем в коллекцию.
             mySearchControl.events.add('resultselect', function (e) {
                 var index = e.get('index');
+                console.log(e);
                 mySearchControl.getResult(index).then(function (res) {
+                    mySearchResults.removeAll();
                     mySearchResults.add(res);
                 });
             }).add('submit', function () {
                 mySearchResults.removeAll();
-            })
+            });
         });
+
+        var setLatLngAttributes = function (point) {
+            $(options.latitude).val(point.lat());
+            $(options.longitude).val(point.lng());
+        };
     };
 })(jQuery);
